e7a165abf1c125e808c0e4e9970768c4cc84035e
Peng Hu <xiaomo@xiaomo.info>
2022-01-15 08:35:39 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # infrastructure-modules/app/main.tf
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
   # ... other provider settings ...
 }
 terraform {
   backend "s3" {}
 }
 module "app" {
-  source = "../../../app"
+  source = ./app"
   instance_type  = var.instance_type
   instance_count = var.instance_count
   # ... other app settings ...
